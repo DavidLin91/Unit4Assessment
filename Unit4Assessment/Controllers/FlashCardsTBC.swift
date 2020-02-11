359f84cd5a42f4a27a0316235370e91d8994b544
David <lin.david985@gmail.com>
2020-02-11 10:16:39 -0500

    setup layout for project

---
+++ Unit4Assessment/Controllers/FlashCardsTBC.swift
@@ -0,0 +1,20 @@
+//
+//  FlashCardsTBC.swift
+//  Unit4Assessment
+//
+//  Created by David Lin on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class FlashCardsTBC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ Unit4Assessment/Views/FlashCardsView.swift
@@ -0,0 +1,32 @@
+//
+//  FlashCardsView.swift
+//  Unit4Assessment
+//
+//  Created by David Lin on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class FlashCardsView: UIView {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder:coder)
+        commonInit()
+    }
+    
+    
+    private func commonInit() {
+        
+    }
+    
+    
+}
+
+
